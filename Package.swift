40c48ac75f2c6e95950a96d67cb317d1a65efe9a
Carlos Simon <git@csimon.dev>
2019-12-06 19:11:38 +0100

    add product

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "localizable-merger",
+    products: [
+        .executable(name: "localizable-merger", targets: ["localizable-merger"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(
